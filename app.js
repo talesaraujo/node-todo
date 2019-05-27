5ea26713b7711a080fdf93688f044a8077680029
talesaraujo <talesaraujodossantos@gmail.com>
2019-05-27 01:54:12 -0300

    db connect

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
-const express = require('express');
-const bodyParser = require('body-parser');
+const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
 
 app.use(bodyParser.json());
+
+const path = require("path");
+const db = require("./db");
+const collection = "todo";
---
+++ db.js
@@ -0,0 +1,27 @@
+const MongoClient = require("mongodb").MongoClient;
+const ObjectID = require("mongodb").ObjectID;
+const dbname = "crud_mongodb";
+const url = "mongodb://localhost:27017";
+
+const mongoOptions = {useNewUrlParser : true};
+const state = {
+    db : null
+};
+
+
+const connect = (cb) => {
+    if (state.db) {
+        cb.db();
+    }
+    else {
+        MongoClient.connect(url, mongoOptions, (err, client) => {
+            if (err) {
+                cb(err);
+            }
+            else {
+                state.db = client.db(dbname);
+                cb();
+            }
+        });
+    }
+}
