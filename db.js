88cde36837e3abdfd30dda395cdc9912ebfca453
talesaraujo <talesaraujodossantos@gmail.com>
2019-06-06 23:48:02 -0300

    Added get primary key and DB methods

--- db.js
+++ db.js
@@ -25,3 +25,13 @@ const connect = (cb) => {
         });
     }
 }
+
+const getPrimaryKey = (_id) => {
+    return ObjectID(_id);
+}
+
+const getDB = () => {
+    return state.db;
+}
+
+module.exports = {getDB, connect, getPrimaryKey};
