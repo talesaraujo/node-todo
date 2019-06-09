af8e85937179786d8905a74a68aee5f4afa66e23
talesaraujo <talesaraujodossantos@gmail.com>
2019-06-09 04:18:37 -0300

    Minor change

--- db.js
+++ db.js
@@ -1,6 +1,6 @@
 const MongoClient = require("mongodb").MongoClient;
 const ObjectID = require("mongodb").ObjectID;
-const dbname = "crud_mongodb";
+const dbname = "todo_mongodb";
 const url = "mongodb://localhost:27017";
 
 const mongoOptions = {useNewUrlParser : true};
