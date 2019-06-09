519b69b770f0e05fd593dac80777ba6e0719116e
talesaraujo <talesaraujodossantos@gmail.com>
2019-06-09 18:47:32 -0300

    Added PUT method

--- app.js
+++ app.js
@@ -25,6 +25,27 @@ app.get('/getTodos', (req, res) => {
     });
 });
 
+/*
+    Server side update portion
+ */
+app.put('/:id', (req, res) => {
+    const todoID = req.params.id;
+    const userInput = req.body;
+
+    db.getDB().collection(collection).findOneAndUpdate({_id: db.getPrimaryKey(todoID)}, 
+                                                     {$set: {todo: userInput.todo}},
+                                                     {returnOriginal: false}, 
+                                                     (err, result) => {
+        if (err) {
+            console.log(err);
+        }
+        else {
+            res.json(result);
+        }
+    });
+});
+
+
 
 db.connect((err) => {
     if (err) {
